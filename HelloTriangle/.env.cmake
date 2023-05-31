a82bf4539fb10fcebdaadab01a73bf8eb1e966d8
Jooh <mnh513@naver.com>
2023-06-01 00:13:39 +0900

    tutorial -> HelloTriangle

--- HelloTriangle/.env.cmake
+++ HelloTriangle/.env.cmake
@@ -1,6 +1,6 @@
-set(GLFW_PATH D:/work/littleVulkanEngine/external_lib/glfw-3.3.8.bin.WIN64)
-set(GLM_PATH D:/work/littleVulkanEngine/external_lib/glm)
-set(VULKAN_SDK_PATH  C:/VulkanSDK/1.3.246.1)
+set(GLFW_PATH C:/work/vulkan-tutorial/glfw-3.3.8.bin.WIN64)
+set(GLM_PATH C:/work/vulkan-tutorial/glm)
+set(VULKAN_SDK_PATH  C:/VulkanSDK/1.3.250.0)
 
 # Set MINGW_PATH if using mingwBuild.bat and not VisualStudio20XX
 # set(MINGW_PATH "C:/Program Files/mingw-w64/x86_64-8.1.0-posix-seh-rt_v6-rev0/mingw64")
